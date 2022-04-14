6b5ed173e81a2b97cdecae87e15668ffb5cc60d5
SandeMiriam <miriamsandeoyuga@gmail.com>
2022-04-14 09:27:28 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var amount=("2000")
+
+
+}
+class CurrentAccount(var Accountnumber:String,Accountname:String,balance:Int)
+{
+    fun deposit(amount:Double){
+    }
+    fun withdrawal(amount: Double){
+
+    }
+    fun details("Accountnumber x with balance y is operated by z")
+}
+class SavingsAccount(var Accountnumber:String,Accountname:String,balance:Int,withdrawals:Int)
+{
+var withdraw=>4
+        withdrawal++
+}
+class hygiene(var name:String,var weight:Int,var price:Int,var category:String)
